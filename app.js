0194f5eb89dcf1203234b2db3a547f0e0374b477
Jean-Baptiste Dominguez <jb@bitcoin.com>
2018-11-07 15:02:01 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
 var spawn = require('child_process').spawn;
 var async = require('async');
 
-var scripts = ['locker/locker.js', 'messagebroker/messagebroker.js',
-  'bcmonitor/bcmonitor.js', 'emailservice/emailservice.js',
+var scripts = ['locker/locker.js', 'messagebroker/messagebroker.js', 'emailservice/emailservice.js',
   'pushnotificationsservice/pushnotificationsservice.js',
   'fiatrateservice/fiatrateservice.js', 'bws.js'];
 
